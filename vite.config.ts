18edcc5452ab7e3a49b52852a88e398de147a671
Zakariya Isah Zakariya <zakariyaisah@gmail.com>
2026-09-15 20:34:08 +0100

    Prepare site for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: '.bolt/public',
+  publicDir: 'public',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
